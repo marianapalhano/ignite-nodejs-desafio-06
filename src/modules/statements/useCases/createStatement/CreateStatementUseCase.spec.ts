ca02a44c086fa9e27d58df5389fde5ddb215bd84
Mariana <marianaufc@gmail.com>
2023-04-14 20:38:05 -0300

    feat(testes-unitarios): adiciona teste de CreateStamentUseCase com saque

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -21,7 +21,7 @@ describe("Create Statement", () => {
         );
     });
 
-    it("should be able to create a statement", async () => {
+    it("should be able to make a deposit", async () => {
         const user = await inMemoryUsersRepository.create({
             name: "User name",
             email: "User email",
@@ -41,4 +41,32 @@ describe("Create Statement", () => {
 
         expect(savedStatement).toHaveProperty("id");
     });
+
+    it("should be able to make a withdraw", async () => {
+      const user = await inMemoryUsersRepository.create({
+          name: "User name",
+          email: "User email",
+          password: "User password",
+      });
+      const depositStatement = {
+        user_id: user.id || "",
+        type: OperationType.DEPOSIT,
+        amount: 200,
+        description: "Deposit Test",
+    };
+      const statement = {
+          user_id: user.id || "",
+          type: OperationType.WITHDRAW,
+          amount: 99,
+          description: "Withdraw Test",
+      };
+      await createStatementUseCase.execute(depositStatement);
+      const createdStatement = await createStatementUseCase.execute(statement);
+      const savedStatement = await inMemoryStatementsRepository.findStatementOperation({
+          statement_id: createdStatement.id || "",
+          user_id: createdStatement.user_id
+      });
+
+      expect(savedStatement).toHaveProperty("id");
+  });
 });
