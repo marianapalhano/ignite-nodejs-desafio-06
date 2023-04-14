e185171a4ea456b67b2c6ca0497c3f61ad691053
Mariana <marianaufc@gmail.com>
2023-04-14 11:31:03 -0300

    feat(testes-unitarios): adiciona teste de getBalanceUseCase

---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+
+let getBalanceUseCase: GetBalanceUseCase;
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+describe("List Statement Operations", () => {
+    beforeEach(() => {
+        inMemoryStatementsRepository = new InMemoryStatementsRepository();
+        inMemoryUsersRepository = new InMemoryUsersRepository();
+        getBalanceUseCase = new GetBalanceUseCase(
+            inMemoryStatementsRepository,
+            inMemoryUsersRepository
+        );
+    });
+
+    it("should be able to list all statement operations of a user and balance", async () => {
+        const user = await inMemoryUsersRepository.create({
+            name: "Mock User",
+            email: "mock@email.com",
+            password: "Password",
+        });
+
+        const statement = await inMemoryStatementsRepository.create({
+          user_id: user.id || "",
+          type: OperationType.WITHDRAW,
+          amount: 99,
+          description: "Mock withdraw"
+        });
+
+        const operations = await getBalanceUseCase.execute({
+          user_id: user.id || ""
+        });
+        expect(operations).toHaveProperty("balance");
+        expect(operations.statement).toEqual([statement]);
+    });
+});
